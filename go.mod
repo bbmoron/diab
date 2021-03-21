38fceb25e6db5d5a6dc6b8a7333beec7aa3eb8ea
Alexandr Romanov <bbmxrxn@gmail.com>
2021-03-21 23:05:11 +0300

    wow it is done;

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.21.3
+	gorm.io/gorm v1.21.4
 )
--- routes/NewUser.go
+++ routes/NewUser.go
@@ -2,15 +2,22 @@ package diab
 
 import (
 	schemas "diab/database"
-	"encoding/json"
 	"encoding/hex"
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"crypto/sha256"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
+
+type docsChats struct {
+	DID    int
+	Amount int
+	Ready  bool
+}
 
 // NewSHA256 handles sha256-ing passwords
 func NewSHA256(data []byte) []byte {
@@ -31,6 +38,36 @@ func NewUser(c *gin.Context, db *gorm.DB) {
 		Password: hex.EncodeToString(shaed),
 	}
 	db.Create(&user)
+	var doctors []schemas.Doctor
+	var chats []schemas.Chat
+	var docIDs []int
+	var dchats []docsChats
+	db.Find(&doctors)
+	for i := range doctors {
+		docIDs = append(docIDs, int(doctors[i].ID))
+		ready, _ := strconv.ParseBool(doctors[i].Ready)
+		dchats = append(dchats, docsChats{DID: int(doctors[i].ID), Amount: 0, Ready: ready})
+	}
+	db.Where(map[string]interface{}{"doctorId": docIDs}).Find(&chats)
+	for i := range doctors {
+		for y := range chats {
+			did, _ := strconv.ParseInt(chats[y].DoctorID, 10, 64)
+			if int(did) == int(doctors[i].ID) {
+				dchats[i].Amount = dchats[i].Amount + 1
+			}
+		}
+	}
+	min := []int{0, dchats[0].Amount}
+	for i := range dchats {
+		if min[1] > dchats[i].Amount {
+			min = []int{i, dchats[i].Amount}
+		}
+	}
+	chat := schemas.Chat{
+		PatientID: strconv.Itoa(int(user.ID)),
+		DoctorID:  strconv.Itoa(dchats[min[0]].DID),
+	}
+	db.Create(&chat)
 	response, _ := json.Marshal(user)
 	c.String(http.StatusOK, string(response))
 }
